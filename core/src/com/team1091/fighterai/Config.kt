64ba5d28c58de587c74dcc66c7cc6a666fb56314
stewsters <stewsters@gmai.com>
2020-10-25 19:55:48 -0500

    Adding my prototype

--- core/src/com/team1091/fighterai/Config.kt
+++ core/src/com/team1091/fighterai/Config.kt
@@ -1,13 +1,14 @@
 package com.team1091.fighterai
 
+import com.team1091.fighterai.actor.pilot.AdrianPrototypePilot
 import com.team1091.fighterai.actor.pilot.SampleKotlinPilot
 
 // This will be where we set out default ai class.  Try not to commit this file.
 // Construct your ship ai here. This will probably look like   { YourClassname() }
-val buildPlayerShip = { SampleKotlinPilot() }
+val buildPlayerShip = { AdrianPrototypePilot() }
 
 // This constructs an opposition ai.  Right now it uses your own, but we can set it to vs others later in the dev process
-val buildAdversarialShip = buildPlayerShip
+val buildAdversarialShip = { SampleKotlinPilot() }
 
 val chaseCamera = true
 
---
+++ core/src/com/team1091/fighterai/actor/pilot/AdrianPrototypePilot.kt
@@ -0,0 +1,175 @@
+package com.team1091.fighterai.actor.pilot
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.math.MathUtils.degreesToRadians
+import com.badlogic.gdx.math.Vector3
+import com.team1091.fighterai.actor.Radar
+import com.team1091.fighterai.actor.RadarContact
+import com.team1091.fighterai.actor.Telemetry
+import com.team1091.fighterai.math.angleBetween
+import com.team1091.fighterai.math.leadTarget
+import com.team1091.fighterai.math.turnTowards
+import com.team1091.fighterai.types.forward
+import com.team1091.fighterai.types.up
+
+/*
+ This is an example ai pilot
+ */
+class AdrianPrototypePilot : Pilot {
+
+    var lastTarget: RadarContact? = null
+    var lastTargetTime: Long = 0
+    var mode: AiState = AiState.ATTACK
+    val groundProximityWarning = 100
+    val optimalHeight = 300
+
+    override fun fly(us: Telemetry, radar: Radar): PilotControl {
+
+        // Are we diving into the ground?  Lets not.
+        if (us.position.z < groundProximityWarning) {
+            // figure out how we are rotated, and which way up is.
+            return pullUp(us)
+        }
+
+        val target: RadarContact? = acquireTarget(radar, us)
+
+        if (target == null) {
+            // There are no targets around, get above optimal height
+
+            if (us.position.z < optimalHeight)
+                return pullUp(us)
+            else
+                return flyTowards(us, forward)// if we dont have a target, get in formation?
+        }
+
+        val dist = us.position.dst(target.position)
+
+        // If in attack mode and we are too close, change to retreat mode.
+        // if we are in retreat mode and too far away, switch to attack mode
+        val towards = when (mode) {
+            AiState.ATTACK -> {
+                if (dist < 100f)
+                    mode = AiState.RETREAT
+                1f
+            }
+            AiState.RETREAT -> {
+                if (dist > 300f)
+                    mode = AiState.ATTACK
+                -1f
+            }
+            AiState.EVADE -> {
+                0f
+            }
+        }
+
+        // else calculate where they will be, and fly towards that location
+        val solution = leadTarget(
+                us.position,
+                target.position,
+                Vector3(0f, target.velocity, 0f).mul(target.rotation),
+                us.velocity + us.primaryWeaponVelocity
+        )
+
+        val unRotatedTargetOffset = solution.path.mul(us.rotation.cpy().conjugate())
+
+        val angle = angleBetween(forward, unRotatedTargetOffset)
+        val power = if (angle < 0.3) {
+            angle * (1f / 0.3f)
+        } else 1f
+
+        val (pitch, yaw, roll) = turnTowards(unRotatedTargetOffset)
+
+
+        var primary = false
+        var secondary = false
+        val accel: Float
+        if (unRotatedTargetOffset.y > 0) { // They are in front of  us
+
+            // if we are close enough, shoot
+            if (us.secondaryWeaponAmmo > 0 && angle < 10 * degreesToRadians && dist < us.secondaryWeaponDuration * us.secondaryWeaponVelocity) {
+                secondary = true
+            }
+
+            if (dist < us.primaryWeaponDuration * us.primaryWeaponVelocity && mode == AiState.ATTACK) {
+                accel = 0.25f
+                primary = us.primaryWeaponAmmo > 0
+
+            } else {
+                accel = 1f
+            }
+
+        } else {
+            accel = 1f
+        }
+
+        return PilotControl(
+                pitch = pitch * towards * power,
+                yaw = yaw * towards * power,
+                roll = roll * towards * power,
+                throttle = accel,
+                primaryWeapon = primary,
+                secondaryWeapon = secondary
+        )
+
+    }
+
+    private fun acquireTarget(radar: Radar, us: Telemetry): RadarContact? {
+        val time = System.currentTimeMillis()
+        val target: RadarContact?
+
+        // find a target, close and in front of us are good ideas
+        if (lastTargetTime + 1000 < time) {
+            target = radar.contacts
+                    .filter {
+                        us.faction.isEnemy(it.faction)
+                    }
+                    .minByOrNull {
+                        it.position.dst(us.position)
+                    }
+
+            lastTarget = target
+            lastTargetTime = time
+        } else {
+            target = lastTarget
+        }
+        return target
+    }
+
+    private fun flyTowards(us: Telemetry, heading: Vector3): PilotControl {
+
+        val localUp = heading.cpy().mul(us.rotation.cpy().conjugate())
+        val (pitch, yaw, roll) = turnTowards(localUp)
+
+        return PilotControl(
+                pitch = pitch,
+                yaw = yaw,
+                roll = roll,
+                throttle = 1f,
+                primaryWeapon = false,
+                secondaryWeapon = false
+        )
+
+    }
+
+    private fun pullUp(us: Telemetry): PilotControl {
+        val localUp = up.cpy().mul(us.rotation.cpy().conjugate())
+        val (pitch, yaw, roll) = turnTowards(localUp)
+
+        Gdx.app.log(us.callsign, "Emergency pull up")
+        return PilotControl(
+                pitch = pitch,
+                yaw = yaw,
+                roll = roll,
+                throttle = 1f,
+                primaryWeapon = false,
+                secondaryWeapon = false
+        )
+    }
+
+}
+
+enum class AiState {
+    ATTACK,
+    RETREAT,
+    EVADE,
+}
